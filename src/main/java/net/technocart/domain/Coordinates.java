654f2b9d239441955b6e10bcca10102b38936734
Shankar <mshankarrao1970@gmail.com>
2018-03-31 21:08:20 -0400

    adding annotations for the entity

--- src/main/java/net/technocart/domain/Coordinates.java
+++ src/main/java/net/technocart/domain/Coordinates.java
@@ -1,14 +1,25 @@
 package net.technocart.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by shankarraomata on 5/21/17.
  */
+@Entity
+@Table(name = "coordinates")
 public class Coordinates {
 
+    @Id
+    @Column(name = "id")
     private String vehicleCode;
 
+    @Column(name = "longitude")
     private Double longitude;
 
+    @Column(name="latitude")
     private Double latitude;
 
     public String getVehicleCode() {
